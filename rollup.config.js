86ca68ca8ee555f527c21a752b208aedda044d21
Takahito Urai <uraitakahito@gmail.com>
2024-11-06 06:29:54 +0900

    Clarify input and output requirements in Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
 // A Rollup plugin to convert CommonJS modules to ES6
 import commonjs from 'rollup-plugin-commonjs'
 
+// can be an array (for multiple inputs)
 export default {
-  input: 'src/index.js',
+  input: 'src/index.js', // conditionally required
+
+  // required (can be an array, for multiple outputs)
   output: {
     file: 'dist/bundle.js',
     format: 'umd',
