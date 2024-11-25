12cac75f7630531bc49683e588aea11f4e4c188b
Takahito Urai <uraitakahito@gmail.com>
2024-11-25 15:21:48 +0900

    Refactor Rollup configuration to add separate debug and default configurations

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,10 @@
-// A Rollup plugin to convert CommonJS modules to ES6
-import commonjs from 'rollup-plugin-commonjs'
-// The @rollup/plugin-node-resolve plugin teaches Rollup how to find external modules.
-import resolve from '@rollup/plugin-node-resolve';
+import defaultConfig from './rollup.default.config.js';
+import debugConfig from './rollup.debug.config.js';
 
-// can be an array (for multiple inputs)
-export default [
-  {
-    input: 'src/main-a.js', // conditionally required
-
-    // required (can be an array, for multiple outputs)
-    output: [
-      {
-        dir: 'dist/es6',
-        format: 'es',
-      },
-      {
-        dir: 'dist/umd',
-        format: 'umd',
-      },
-    ],
-    plugins: [
-      commonjs(),
-      resolve()
-    ]
-  },
-  {
-    input: 'src/main-b.js', // conditionally required
-
-    // required (can be an array, for multiple outputs)
-    output: [
-      {
-        dir: 'dist/es6',
-        format: 'es',
-      },
-    ]
-  }
-];
+export default commandLineArgs => {
+  // If the command line argument --config-debug | --configDebug is set, use the debug configuration
+	if (commandLineArgs.configDebug === true) {
+		return debugConfig;
+	}
+	return defaultConfig;
+};
---
+++ rollup.default.config.js
@@ -0,0 +1,39 @@
+// A Rollup plugin to convert CommonJS modules to ES6
+import commonjs from 'rollup-plugin-commonjs'
+// The @rollup/plugin-node-resolve plugin teaches Rollup how to find external modules.
+import resolve from '@rollup/plugin-node-resolve';
+
+// can be an array (for multiple inputs)
+export default [
+  {
+    input: 'src/main-a.js', // conditionally required
+
+    // required (can be an array, for multiple outputs)
+    output: [
+      {
+        dir: 'dist/es6',
+        format: 'es',
+      },
+      {
+        dir: 'dist/umd',
+        format: 'umd',
+        name: 'MyModule',
+      },
+    ],
+    plugins: [
+      commonjs(),
+      resolve()
+    ]
+  },
+  {
+    input: 'src/main-b.js', // conditionally required
+
+    // required (can be an array, for multiple outputs)
+    output: [
+      {
+        dir: 'dist/es6',
+        format: 'es',
+      },
+    ]
+  }
+];
