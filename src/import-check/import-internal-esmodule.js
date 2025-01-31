5a82b8e2954d43e876d1adcf020653ca6a468086
Takahito Urai <uraitakahito@gmail.com>
2025-02-01 07:42:32 +0900

    Disable console warnings in import check files

--- src/import-check/import-internal-esmodule.js
+++ src/import-check/import-internal-esmodule.js
@@ -1,4 +1,7 @@
 /* eslint-disable import/no-duplicates */
+/* eslint-disable no-console */
+/* eslint-disable no-duplicate-imports */
+/* eslint-disable sort-imports */
 
 // When importing ES modules without using a bundler or transpiler, file extensions are required:
 //   https://nodejs.org/api/esm.html#esm_mandatory_file_extensions
--- src/import-check/suppress-warning.js
+++ src/import-check/suppress-warning.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import hello from '@uraitakahito/hello-esmodule';
 
 console.log(hello.say());
